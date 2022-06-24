dd0e3764d584d38e6596eb58ca7d5b54c03f8805
manjukalal <manjukalal1496@gmail.com>
2022-06-24 20:11:14 +0530

    Modified:removed avoidable word from the pharse

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -4,9 +4,13 @@ import java.util.Hashtable;
 
 class HashTable {
     public static void main(String[] args) {
-        String str = " “Paranoids are not paranoid because they are paranoid but because " +
-                "they keep putting themselves deliberately into paranoid avoidable situations";
+
         Hashtable<String, Integer> hashtable = new Hashtable<>();
+
+        String str = " Paranoids are not paranoid because they are paranoid" +
+                " but because they keep putting themselves deliberately into paranoid avoidable situations";
+
+        str = str.replace("avoidable"," ");
 
         String[] words = str.split(" ");
 
